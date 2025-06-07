5960a4890f7ec2b380e2ae962a42156ccb80c64f
Naveen Soni <naveennsonii@gmail.com>
2025-06-07 21:46:57 +0530

    style: loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,11 @@ import Lottie from "lottie-react";
 import loadingAnimation from "../animations/LoadAnimation.json";
 
 const Loader = () => {
-  return <Lottie animationData={loadingAnimation} />;
+  return (
+    <div className="-mt-32 sm:-mt-28">
+      <Lottie animationData={loadingAnimation} />
+    </div>
+  );
 };
 
 export default Loader;
