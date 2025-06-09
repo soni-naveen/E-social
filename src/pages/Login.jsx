e7bb9552e025b22d656865086e7fca794fad5552
Naveen Soni <naveennsonii@gmail.com>
2025-06-09 22:34:58 +0530

    style: login signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Loader from "../components/Loader";
+import { AnimatePresence, motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 export default function Login() {
   const ENDPOINT = import.meta.env.VITE_REACT_BASE_URL;
@@ -55,6 +57,8 @@ export default function Login() {
     }
   };
 
+  const location = useLocation();
+
   return (
     <>
       {loading ? (
@@ -66,67 +70,77 @@ export default function Login() {
         </div>
       ) : (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-teal-100 via-cyan-100 to-white py-12 px-3 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8 p-8 sm:p-10 backdrop-blur-md shadow-lg bg-slate-100/80 rounded-lg">
-            <div className="pl-1 text-2xl sm:text-3xl text-teal-600">
-              <p className="text-xs sm:text-sm font-semibold">E-Social</p>
-              <p className="font-semibold mt-2">Welcome Back!</p>
-              <p className="text-xs sm:text-sm mt-1 text-neutral-700">
-                Sign in to access your account.
-              </p>
-            </div>
-
-            <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-              <div className="rounded-sm shadow-sm space-y-4">
-                <div>
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    required
-                    value={formData.username}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Username"
-                  />
-                </div>
-                <div>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Password"
-                  />
-                </div>
-              </div>
-
-              {error && (
-                <p className="text-red-500 text-xs sm:text-sm mt-2">{error}</p>
-              )}
-              {successMessage && (
-                <p className="text-green-500 text-xs sm:text-sm mt-2">
-                  {successMessage}
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] }}
+              className="max-w-md w-full space-y-8 p-8 sm:p-10 backdrop-blur-md shadow-lg bg-slate-100/80 rounded-lg"
+            >
+              <div className="pl-1 text-2xl sm:text-3xl text-teal-600">
+                <p className="text-xs sm:text-sm font-semibold">E-Social</p>
+                <p className="font-semibold mt-2">Welcome Back!</p>
+                <p className="text-xs sm:text-sm mt-1 text-neutral-700">
+                  Sign in to access your account.
                 </p>
-              )}
-              <div>
-                <button
-                  type="submit"
-                  className="w-full mx-auto flex justify-center py-2 px-4 border border-transparent text-xs sm:text-sm font-semibold rounded-md text-white bg-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
-                >
-                  LOGIN
-                </button>
               </div>
-            </form>
-            <div className="text-gray-500 text-xs sm:text-sm">
-              Don’t have an account?{" "}
-              <Link to="/register" className="underline text-blue-600">
-                Create now
-              </Link>
-            </div>
-          </div>
+              <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+                <div className="rounded-sm shadow-sm space-y-4">
+                  <div>
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      required
+                      value={formData.username}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+
+                {error && (
+                  <p className="text-red-500 text-xs sm:text-sm mt-2">
+                    {error}
+                  </p>
+                )}
+                {successMessage && (
+                  <p className="text-green-500 text-xs sm:text-sm mt-2">
+                    {successMessage}
+                  </p>
+                )}
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full mx-auto flex justify-center py-2 px-4 border border-transparent text-xs sm:text-sm font-semibold rounded-md text-white bg-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
+                  >
+                    LOGIN
+                  </button>
+                </div>
+              </form>
+              <div className="text-gray-500 text-xs sm:text-sm">
+                Don’t have an account?{" "}
+                <Link to="/register" className="underline text-blue-600">
+                  Create now
+                </Link>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
       )}
     </>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Loader from "../components/Loader";
+import { AnimatePresence, motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 export default function Signup() {
   const ENDPOINT = import.meta.env.VITE_REACT_BASE_URL;
@@ -79,6 +81,8 @@ export default function Signup() {
     }
   };
 
+  const location = useLocation();
+
   return (
     <>
       {loading ? (
@@ -89,94 +93,104 @@ export default function Signup() {
           </div>
         </div>
       ) : (
-        <div className="h-[calc(100vh)] flex items-center justify-center bg-gradient-to-br from-teal-100 via-cyan-100 to-white py-12 px-3 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8 p-8 sm:p-10 backdrop-blur-md shadow-lg bg-slate-100/60 rounded-lg">
-            <div className="pl-1 text-2xl sm:text-3xl text-teal-600">
-              <p className="text-xs sm:text-sm font-semibold">E-Social</p>
-              <p className="font-semibold mt-2">Create Account</p>
-              <p className="text-xs sm:text-sm mt-1 text-neutral-700">
-                Connect with friends, meet new ones, and share moments.
-              </p>
-            </div>
-
-            <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-              <div className="rounded-sm space-y-4">
-                <div>
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    required
-                    pattern="^[a-z0-9_\-]{3,20}$"
-                    value={formData.username}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Username"
-                  />
-                </div>
-                <div>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Email address"
-                  />
-                </div>
-                <div>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Password"
-                  />
-                </div>
-                <div>
-                  <input
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    type="password"
-                    required
-                    value={formData.confirmPassword}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
-                    placeholder="Confirm Password"
-                  />
-                </div>
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-teal-100 via-cyan-100 to-white py-12 px-3 sm:px-6 lg:px-8">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] }}
+              className="max-w-md w-full space-y-8 p-8 sm:p-10 backdrop-blur-md shadow-lg bg-slate-100/80 rounded-lg"
+            >
+              <div className="pl-1 text-2xl sm:text-3xl text-teal-600">
+                <p className="text-xs sm:text-sm font-semibold">E-Social</p>
+                <p className="font-semibold mt-2">Create Account</p>
+                <p className="text-xs sm:text-sm mt-1 text-neutral-700">
+                  Connect with friends, meet new ones, and share moments.
+                </p>{" "}
               </div>
+              <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+                <div className="rounded-sm space-y-4">
+                  <div>
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      required
+                      pattern="^[a-z0-9_\-]{3,20}$"
+                      value={formData.username}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Username"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      required
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Email address"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      type="password"
+                      required
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 text-gray-900 rounded-sm focus:outline-none text-sm sm:text-base placeholder:text-neutral-400"
+                      placeholder="Confirm Password"
+                    />
+                  </div>
+                </div>
 
-              {error && (
-                <p className="text-red-500 text-xs sm:text-sm mt-2">{error}</p>
-              )}
-              {successMessage && (
-                <p className="text-green-500 font-semibold text-xs sm:text-sm mt-2">
-                  {successMessage}
-                </p>
-              )}
+                {error && (
+                  <p className="text-red-500 text-xs sm:text-sm mt-2">
+                    {error}
+                  </p>
+                )}
+                {successMessage && (
+                  <p className="text-green-500 font-semibold text-xs sm:text-sm mt-2">
+                    {successMessage}
+                  </p>
+                )}
 
-              <div>
-                <button
-                  type="submit"
-                  className="w-full mx-auto flex justify-center py-2 px-4 border border-transparent text-xs sm:text-sm font-semibold rounded-md text-white bg-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
-                >
-                  SIGN UP
-                </button>
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full mx-auto flex justify-center py-2 px-4 border border-transparent text-xs sm:text-sm font-semibold rounded-md text-white bg-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-600"
+                  >
+                    SIGN UP
+                  </button>
+                </div>
+              </form>
+              <div className="text-gray-500 text-xs sm:text-sm">
+                Already have account?{" "}
+                <Link to="/login" className="underline text-blue-600">
+                  Login here
+                </Link>
               </div>
-            </form>
-            <div className="text-gray-500 text-xs sm:text-sm">
-              Already have account?{" "}
-              <Link to="/login" className="underline text-blue-600">
-                Login here
-              </Link>
-            </div>
-          </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
       )}
     </>
